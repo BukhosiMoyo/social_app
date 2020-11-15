f609d7f4747da58f4e8cc72d1a2390842f470961
Bukhosi Moyo <bukhosizimcode@gmail.com>
2020-11-15 11:50:24 +0200

    Adding fully functional login and log out

--- social_app/settings.py
+++ social_app/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 #Authentication settings
 
-LOGIN_REDIRECT_URL =    'dashboard'
+LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
--- social_app/urls.py
+++ social_app/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls')),# Create your views here.
-# Create your views here.
+    path('account/', include('accounts.urls')),
 
 ]
