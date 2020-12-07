2bd6a798043c3573d579d278cb0647b5547a723b
Bukhosi Moyo <bukhosizimcode@gmail.com>
2020-12-07 14:22:15 +0200

    Adding image likes to the image model and registering to the admin

--- images/models.py
+++ images/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Image(models.Model):
+    """Storing the image information"""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="images_created", on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=200, blank=True)
+    url = models.URLField()
+    image = models.ImageField(upload_to="inames/%Y/%m/%d")
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True, db_index=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+
+    def __str__(self):
+        """Change to human string for human readability"""
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    
